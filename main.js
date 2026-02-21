82f65481923e12fea5b0e8a951e5268277a7b7b6
bc2025-2 <Anastasiia.Samosuieva@lnu.edu.ua>
2026-02-21 17:11:30 +0200

    Final

---
+++ main.js
@@ -0,0 +1,10 @@
+const date = "20231001";
+const time_period = "q";
+
+const url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/basindbank");
+
+url.searchParams.append("date", date);
+url.searchParams.append("period", time_period);
+url.searchParams.append("json", "");
+
+console.log(url.toString());
